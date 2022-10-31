adb611169b0d4f86206a482cb5373c9fccc37d5d
badsnus <badsnus@mail.ru>
2022-10-31 20:56:42 +0500

    added README

--- ELONSHOP/django_shop/settings.py
+++ ELONSHOP/django_shop/settings.py
@@ -15,7 +15,7 @@ env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY', default='NO_SECRET_KEY')
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
